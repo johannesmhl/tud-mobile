1682d465009cfdade1e10e4978e0253c0a35296c
Andrew_Borodaev <borodaev17@gmail.com>
2023-12-11 15:13:29 +0000

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  static const String _title = 'Flutter Stateful Clicker Counter';
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+  static const String _title = 'TUD Mobile';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,78 +13,113 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/page1': (context) => const TimeManagmentPage(),
+        '/page2': (context) => const MeinStudiumPage(),
+        '/page3': (context) => const NewsPage(),
+        '/page4': (context) => const PageFour(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // This class is the configuration for the state.
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text('Flutter Demo Click Counter'),
+        title: const Text('TUD Mobile'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: const TextStyle(fontSize: 25),
-            ),
+            buildClickableButton(context, 'Time Managment', '/page1'),
+            buildClickableButton(context, 'Mein Studium', '/page2'),
+            buildClickableButton(context, 'Neuigkeiten', '/page3'),
+            buildClickableButton(context, 'Campus Navigator', '/page4'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+
+  Widget buildClickableButton(BuildContext context, String text, String route) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushNamed(context, route);
+        },
+        child: Text(text, style: const TextStyle(fontSize: 24)), // Text size),
+      ),
+    );
+  }
+}
+
+class TimeManagmentPage extends StatelessWidget {
+  const TimeManagmentPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page One'),
+      ),
+      body: Center(
+        child: const Text('This is Page One'),
+      ),
+    );
+  }
+}
+
+class MeinStudiumPage extends StatelessWidget {
+  const MeinStudiumPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mein Studium'),
+      ),
+      body: Center(
+        child: const Text('This is Page Two'),
+      ),
+    );
+  }
+}
+
+class NewsPage extends StatelessWidget {
+  const NewsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Three'),
+      ),
+      body: Center(
+        child: const Text('This is Page Three'),
+      ),
+    );
+  }
+}
+
+class PageFour extends StatelessWidget {
+  const PageFour({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Page Four'),
+      ),
+      body: Center(
+        child: const Text('This is Page Four'),
       ),
     );
   }
